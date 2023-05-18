42476c33bf9f70e8bf619919f385667114d43093
eunsukimme <eunsu.dev@gmail.com>
2023-05-19 03:49:46 +0900

    fix: fix zsh plugins

--- home.nix
+++ home.nix
@@ -58,22 +58,21 @@
       {
         name = "zsh-powerlevel10k";
         src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
       }
       {
         name = "zsh-fast-syntax-highlighting";
         src = pkgs.zsh-fast-syntax-highlighting;
+        file = "share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh";
       }
       {
         name = "zsh-autosuggestions";
         src = pkgs.zsh-autosuggestions;
+        file = "share/zsh-autosuggestions/zsh-autosuggestions.zsh";
       }
       {
         name = "zsh-completions";
         src = pkgs.zsh-completions;
-      }
-      {
-        name = "zsh-fzf";
-        src = pkgs.oh-my-zsh;
       }
     ];
     oh-my-zsh = {
