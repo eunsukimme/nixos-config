e68ef90a5867970cb860766eadebb94c26218ac2
evan-kim_karrot <evan.kim@daangn.com>
2026-03-15 23:26:38 +0900

    chore: add claude code bin path to environment variables

--- home.nix
+++ home.nix
@@ -115,6 +115,9 @@
     export ANDROID_HOME=$HOME/Library/Android/sdk
     export PATH=$PATH:$ANDROID_HOME/emulator
     export PATH=$PATH:$ANDROID_HOME/platform-tools
+
+    # claude code bin
+    export PATH="$HOME/.local/bin:$PATH"
     '';
     initExtraFirst = ''
       # Powerlevel10k instant prompt
